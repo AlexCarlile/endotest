6d7ba7f24d859765335d3456a1e04d45cb03369b
AlexCarlile <114814856+AlexCarlile@users.noreply.github.com>
2024-03-11 15:31:51 +0300

    Updates

---
+++ precache-manifest.36ee605e23609e93bff1142b81fd4e1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c6200f9e9141e9dfb55cae2ff7af99",
+    "url": "/endotest/index.html"
+  },
+  {
+    "revision": "739cdedbe61268b906e3",
+    "url": "/endotest/static/css/main.d9a64926.chunk.css"
+  },
+  {
+    "revision": "7c333e74c3a1ae420b50",
+    "url": "/endotest/static/js/2.07b24e3a.chunk.js"
+  },
+  {
+    "revision": "0f431788df9fedcd7536120fa5bb2b3b",
+    "url": "/endotest/static/js/2.07b24e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "739cdedbe61268b906e3",
+    "url": "/endotest/static/js/main.4f81f881.chunk.js"
+  },
+  {
+    "revision": "6cb3a5bedca89ad6debc",
+    "url": "/endotest/static/js/runtime-main.5773ac4f.js"
+  },
+  {
+    "revision": "a096107e9fcf2910c7c2e28eb418d502",
+    "url": "/endotest/static/media/Clinic.a096107e.jpg"
+  },
+  {
+    "revision": "a44dbfda974fcd472e70bf50c99b7b00",
+    "url": "/endotest/static/media/Fadeev.a44dbfda.png"
+  },
+  {
+    "revision": "c2dbf7e45352e236bb4f09ebb53b1254",
+    "url": "/endotest/static/media/Fadeev.c2dbf7e4.jpg"
+  },
+  {
+    "revision": "4177b34b74fb4a05721f1908893cbd41",
+    "url": "/endotest/static/media/Group.4177b34b.png"
+  },
+  {
+    "revision": "f4e652b9c6e12a5b61c0cd1d03c73ae3",
+    "url": "/endotest/static/media/Page-title_bckg.f4e652b9.svg"
+  },
+  {
+    "revision": "692fe913e73d5ede1e55dd7f8fa6ef8b",
+    "url": "/endotest/static/media/doctor-performing-routine-medical-checkup 3.692fe913.jpg"
+  },
+  {
+    "revision": "e648621e455e108f5a8bf25d8e320d2a",
+    "url": "/endotest/static/media/logo_sech_white.e648621e.svg"
+  }
+]);
