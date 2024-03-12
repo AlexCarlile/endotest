f4429c4a62f7678581f33add568908c4dcfe2221
AlexCarlile <114814856+AlexCarlile@users.noreply.github.com>
2024-03-12 15:19:49 +0300

    Удалено firebase

--- src/App.js
+++ src/App.js
@@ -7,26 +7,9 @@ import Contacts from "./pages/Contacts";
 import { Routes, Route } from "react-router-dom";
 import HCP from "./pages/HCP";
 import { HCP_list } from "./components/HCPPage/HCP_list";
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 
 export default function App () {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-    apiKey: "AIzaSyCd11lLxG8sb0zPtMZX86qODe5fitki7I4",
-    authDomain: "endocampus-afae4.firebaseapp.com",
-    projectId: "endocampus-afae4",
-    storageBucket: "endocampus-afae4.appspot.com",
-    messagingSenderId: "1073309684018",
-    appId: "1:1073309684018:web:dea11ad030b943fd5ec72b"
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
     return (
         <div className="wrapper">
             <Header />
